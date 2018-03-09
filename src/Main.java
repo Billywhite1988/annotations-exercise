93147a768d2676d3eca4bf943dab5cbcdadef761
Billy <billy_white.88@yahoo.com>
2018-03-09 10:48:16 -0600

    Easy fix

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,20 @@ public class Main {
         author.addBook("Practical Object-Oriented Design in Ruby");
         author.addBook("99 Bottles of OOP");
 
-        for (String book: author.getBooks()) {
+
+        for (String book: author.publishedBooks()) {
             System.out.println(book);
         }
     }
 }
+
+
+
+
+
+//    method does not override or implement a method from a supertype
+//@Override
+//Note: src/Main.java uses or overrides a deprecated API.
+//        Note: Recompile with -Xlint:deprecation for details.
+//        Note: src/Author.java uses unchecked or unsafe operations.
+//        Note: Recompile with -Xlint:unchecked for details.
